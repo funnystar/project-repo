5aa9244f67446adf2c3306d5849f6727e3579cf7
funnystar <993675939@qq.com>
2023-09-19 20:00:25 +0800

    第六次提交

--- src/views/sys/user.vue
+++ src/views/sys/user.vue
@@ -1,15 +1,139 @@
 <template>
     <div>
-        用户管理
+        <!--搜索栏-->
+        <el-card id="search">
+            <el-row>
+                <el-col :span="20">
+                    <el-input v-model="searchModel.username" placeholder="用户名" clearable></el-input>
+                    <el-input v-model="searchModel.phone" placeholder="电话" clearable></el-input>
+                    <el-button @click="getUserList" type="primary" round icon="el-icon-search">查询</el-button>
+                </el-col>
+                <el-col :span="4" align="right">
+                    <el-button @click="openEditUI" type="primary" icon="el-icon-plus" circle></el-button>
+                </el-col>
+            </el-row>
+        </el-card>
+
+        <!-- 结果列表 -->
+            <el-card>
+                <el-table
+        :data="userList"
+        stripe
+        style="width: 100%">
+        <el-table-column type ="index" label="#" width="80">
+            <template slot-scope="scope">
+                <!-- (pageNo-1) * pageSize + index + 1 -->
+                {{ (searchModel.pageNo-1) * searchModel.pageSize + scope.$index + 1 }}
+            </template>
+        </el-table-column>
+        <el-table-column prop="id" label="用户id" width="180">
+        </el-table-column>
+        <el-table-column prop="username" label="用户名" width="180">
+        </el-table-column>
+        <el-table-column prop="phone" label="电话" width="180">
+        </el-table-column>
+        <el-table-column prop="email" label="电子邮件">
+        </el-table-column>
+        <el-table-column label="操作" width="180">
+        </el-table-column>
+      </el-table>
+            </el-card>
+
+            <!-- 分页组件 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="searchModel.pageNo"
+          :page-sizes="[5, 10, 20, 50]"
+          :page-size="searchModel.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+        </el-pagination>
+
+        <!-- 用户信息编辑对话框 -->
+         <el-dialog @close="clearForm" :title="title" :visible.sync="dialogFormVisible">
+            <el-form :model="userForm">
+                <el-form-item label="用户名" :label-width="formLabelWidth">
+                    <el-input v-model="userForm.username" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item type="password" label="用户密码" :label-width="formLabelWidth">
+                    <el-input v-model="userForm.password" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="联系电话" :label-width="formLabelWidth">
+                    <el-input v-model="userForm.phone" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="用户状态" :label-width="formLabelWidth">
+                    <el-switch 
+                        v-model="userForm.status"
+                        :active-value="1"
+                        :inactive-value="0">
+                    </el-switch>
+                </el-form-item>
+                <el-form-item label="电子邮件" :label-width="formLabelWidth">
+                    <el-input v-model="userForm.email" autocomplete="off"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
 <script>
+import userApi from '@/api/userManage'
 export default{
+    data(){
+        return{
+            formLabelWidth:'130px',
+            userForm:{},
+            dialogFormVisible:false,
+            title:"",
+            total:0,
+            searchModel: {
+                pageNo: 1,
+                pageSize: 10
+            },
+            userList:[]
+        }
+    },
+    methods:{
+        clearForm(){
+            this.userForm = {};
+        },
+        handleSizeChange(pageSize){
+            this.searchModel.pageSize = pageSize;
+            this.getUserList();
+        },
+        handleCurrentChange(pageNo){
+            this.searchModel.pageNo = pageNo;
+            this.getUserList();
+        },
+        getUserList(){
+            userApi.getUserList(this.searchModel).then(reponse =>{
+                this.userList = reponse.data.rows;
+                this.total = reponse.data.total;
 
+            });
+        },
+        openEditUI(){
+            this.title = "新增用户";
+            this.dialogFormVisible = true;
+        }
+    },
+    created(){
+        this.getUserList();
+    }
 }
 </script>
 
 <style>
-
+#search .el-input{
+    width:200px;
+    margin-right: 10px;
+}
+.el-dialog .el-input{
+    width:85%;
+}
 </style>
